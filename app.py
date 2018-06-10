c4b01fae5e75c10b33c9ba00f52d8fff5cd96f8c
victor <princeoftennis735@gmail.com>
2018-06-10 13:39:12 +0800

    modify template

--- app.py
+++ app.py
@@ -25,8 +25,9 @@ def showData(sno):
         countDown = countDown-1
 #    d = '2018-06-05'
     sname = crawler.showSingleVal(sno,'sna')
+    addr = crawler.showSingleVal(sno,'ar')
     print(date_list,avg_list)
-    return render_template('visualData.html')
+    return render_template('visualData.html',sname=sname,addr=addr,values=avg_list)
 @app.route('/api/data/<sno>',methods=['GET'])
 def showjson(sno):
     sno = str(sno)
